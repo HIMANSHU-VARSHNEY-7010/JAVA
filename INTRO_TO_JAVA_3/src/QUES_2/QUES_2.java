d7e72e2314b5f5228ae5d2e5e5ff3445a091ddd2
HIMANSHU-VARSHNEY-7010 <himanshu.varshney@tothenew.com>
2023-03-03 13:26:31 +0530

    Java 3

---
+++ INTRO_TO_JAVA_3/src/QUES_2/QUES_2.java
@@ -0,0 +1,61 @@
+package QUES_2;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+//) Create a User class with fields:  firstname, lastname, age, phonenumber.
+// Write a program which accepts values of user fields from commandline,
+// create object and append that to a text file.
+// After every user creation the program should prompt: "Do you want to continue creating users?
+// (Type QUIT to exit)" and keep on accepting values and writing to file unitl user quits.
+public class QUES_2 {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        Scanner scn = new Scanner(System.in) ;
+
+        File file = new File("Details.txt");
+
+        ;
+        // File Writer can take 2 parameters 1st is File argument 2nd is true which append true or false
+        Boolean flag = true;
+        do{
+//            System.out.println("Enter Id ");
+//            int id = scn.nextInt() ;
+
+            System.out.println("Please enter the details :-");
+
+            System.out.println("Enter the FirstName: ");
+            String FirstName = scn.nextLine() ;
+
+            System.out.println("Enter the LastName: ") ;
+            String LastName = scn.nextLine() ;
+
+            System.out.println("Enter the Age: ") ;
+            String age = scn.nextLine() ;
+
+            System.out.println("Enter The Phone Number: ");
+            String phoneNo = scn.nextLine() ;
+
+            SingletonClass s1 = SingletonClass.getInstance(FirstName , LastName , age , phoneNo) ;
+            System.out.println(s1.showDetails());
+
+            System.out.println("Do you want to continue creating users? (Type QUIT to exit):");
+            String strng = scn.nextLine();
+            if(strng.equalsIgnoreCase("QUIT")){
+                flag = false;
+            }
+
+            try(BufferedWriter br = new BufferedWriter(new FileWriter(file,true))){ //
+                br.write(s1.showDetails());
+                br.newLine();
+
+
+            }catch (IOException e){
+                System.out.println("Unable To Read File");
+            }
+
+        }while(flag);
+    }
+}
---
+++ INTRO_TO_JAVA_3/src/QUES_2/SingletonClass.java
@@ -0,0 +1,39 @@
+package QUES_2;
+
+    public class SingletonClass {
+        private static SingletonClass obj ;
+        private  String  FirstName ;
+        private String LastName ;
+        private String age ;
+
+        private String phoneNo ;
+
+        private SingletonClass(String FirstName , String LastName , String age , String phoneNo){
+
+            this.FirstName  = FirstName ;
+            this.LastName = LastName ;
+            this.age = age ;
+            this.phoneNo = phoneNo ;
+        }
+
+        public static SingletonClass getInstance(String FirstName , String LastName , String age , String phoneNo){
+            if(obj == null){
+                obj = new SingletonClass(FirstName , LastName , age , phoneNo) ;
+            }
+            else{
+                obj.FirstName  = FirstName ;
+                obj.LastName = LastName ;
+                obj.age = age ;
+                obj.phoneNo = phoneNo ;
+
+            }
+            return obj ;
+        }
+
+        public String showDetails(){
+        return ("First Name: " + this.FirstName + ", Last Name: " + this.LastName + ", Age: " + this.age +
+                ", Phone No.: " + this.phoneNo);
+        }
+
+    }
+
