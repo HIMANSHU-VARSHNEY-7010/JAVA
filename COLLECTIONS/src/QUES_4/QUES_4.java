f5be9cb616923a69bca8577863089f506f6ec837
HIMANSHU-VARSHNEY-7010 <himanshu.varshney@tothenew.com>
2023-03-05 20:12:16 +0530

    Collection

--- COLLECTIONS/src/QUES_4/QUES_4.java
+++ COLLECTIONS/src/QUES_4/QUES_4.java
@@ -7,9 +7,9 @@ import java.util.Map;
 public class QUES_4 {
     public static void main(String[] args) {
         HashMap<Employee,Integer> map=new HashMap<>();
-        map.put(new Employee("Nitin",22,"Trainee"),200110);
-        map.put(new Employee("Pranshu",22,"Trainee"),29200);
-        map.put(new Employee("Meher",22,"Trainee"),200100);
+        map.put(new Employee("Himanshu",23,"Trainee"),100000);
+        map.put(new Employee("Inder",22,"Trainee"),60000);
+        map.put(new Employee("Devansh",21,"Trainee"),50000);
         for(Map.Entry<Employee,Integer> mp:map.entrySet()){
             System.out.println(mp.getKey() +"|Salary :"+mp.getValue());
         }
